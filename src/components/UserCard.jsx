6f6566c28a6f22f5719cafd808ce82e8f42bba0d
aleksi-svaridze <aleksisvaridze@outlook.com>
2023-11-27 16:52:05 +0400

    Start card component styling

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,9 +1,16 @@
-
+import temporeryUserAvatar from '../images/avatar.png';
 
 const UserCard = ({isDark}) => {
     return(
         <div className={`card ${isDark ? 'card--light' : 'card--dark'}`}>
-            user card here
+            <img src={temporeryUserAvatar} className='card__avatar' alt="" />
+            <div>
+                <div className='card__header'>
+                    <h2 className='card__title'>The Octocat<span>Joined 25 Jan 2011</span></h2>
+                </div>
+                <div className='card__body'></div>
+                <div className='card__footer'></div>
+            </div>
         </div>
     )
 }
